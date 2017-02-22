6b699927608f10d30de8ef0c6e84a943bd7249b5
Christopher Turko <cturko@gmail.com>
2017-02-22 13:02:18 -0700

    Update tiny_url.py

--- tiny_url.py
+++ tiny_url.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/python
 # -*- coding: utf-8 -*-
 import urllib.request
+import sys
 
 def tiny_url(web_address):
     target = 'http://tinyurl.com/api-create.php?url=' + web_address
@@ -8,9 +9,14 @@ def tiny_url(web_address):
     return response.read()
 
 def main():
-    web_address = input("Enter URL: ")
-    print(tiny_url(web_address))
+    while True:
+        web_address = input("Enter URL: ")
+        if web_address == 'quit':
+            sys.exit()
+        else:
+            print(tiny_url(web_address))
 
-    
+
 if __name__ == '__main__':
     main()
+
