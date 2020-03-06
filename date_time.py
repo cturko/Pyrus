b35cd6b89470c3a1ba6aea590e3fa4fdfcacfb82
Christopher Turko <cturko@gmail.com>
2020-03-06 12:59:04 -0700

    Update date_time.py

--- date_time.py
+++ date_time.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# -*_ coding: utf-8 -*-
 import datetime
 import sys
 
@@ -9,32 +7,23 @@ def main():
     now = datetime.datetime.now()
 
     while True:
-        user_request = input("\nCurrent [time, day, date]: ")
 
-        if user_request == "quit":
+        user_request = input("\n Current [time, day, date, or quit]: ")
+
+        if user_request == 'quit':
             sys.exit()
 
-        if user_request == "time":
+        if user_request == 'time':
             second = str(now.second)
-            time_request = (str(now.hour)
-                            + ":"
-                            + str(now.minute)
-                            + ":"
-                            + str(now.second))
-            print("\nIt is: " + time_request)
-
-        if user_request == "day":
-            day_request = str(now.strftime("%A"))
+            time_request = (str(now.hour) + ':' + str(now.minute) + ':' + str(now.second))
+            print ("\nIt is: " + time_request)
+        
+        if user_request == 'day':
+            day_request = str(now.strftime('%A'))
             print("\nIt is " + day_request)
 
-        if user_request == "date":
-            date_request = (str(now.year)
-                            + "-"
-                            + str(now.month)
-                            + "-"
-                            + str(now.day))
+        if user_request == 'date':
+            date_request = (str(now.year) + '-' + str(now.month) + '-' + str(now.day))
             print("\nIt is: " + date_request)
 
-
-if __name__ == '__main__':
-    main()
+main()
