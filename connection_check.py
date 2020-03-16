5ef6fb53f033c2606fa7dce3715d8e89d0088507
Christopher Turko <cturko@gmail.com>
2020-03-16 09:09:02 -0600

    Update connection_check.py

--- connection_check.py
+++ connection_check.py
@@ -1,8 +1,11 @@
 import urllib.request
 
-try:
-    urllib.request.urlopen('https://duckduckgo.com')
-    print("Success: Connected.")
+def check_connection():
+    try:
+        urllib.request.urlopen('https://duckduckgo.com')
+        print("Success: Connected")
 
-except urllib.error.URLError:
-    print("Error: No Connection.")
+    except urllib.error.URLError:
+        print("Error: No Connection")
+
+check_connection()
